2de5087362fb1c543be04181c71d6e494ef6c270
Sutinai Jaradwilaswanit <the-nai@hotmail.com>
2017-06-09 23:44:07 +0700

    Update Child Theme

--- wp-content/themes/Avadachild/functions.php
+++ wp-content/themes/Avadachild/functions.php
@@ -1,5 +1,7 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_enqueue_assets' );
-function my_enqueue_assets() {
-wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles',11 );
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/css/index.css');
+
 }
