51c14e6fbe113a15002c16d76bc0c61465af41c4
Sutinai Jaradwilaswanit <The-nai@hotmail.com>
2017-06-21 02:50:01 +0700

    Update css

--- wp-content/themes/porto-child/functions.php
+++ wp-content/themes/porto-child/functions.php
@@ -9,6 +9,11 @@ function porto_child_css() {
     wp_register_style( 'styles-child', get_stylesheet_directory_uri() . '/style.css' );
     wp_enqueue_style( 'styles-child' );
 
+    //custom css
+    wp_deregister_style( 'styles-custom' );
+    wp_register_style( 'styles-custom', get_stylesheet_directory_uri() . '/css/custom.css' );
+    wp_enqueue_style( 'styles-custom' );
+
     if (is_rtl()) {
         wp_deregister_style( 'styles-child-rtl' );
         wp_register_style( 'styles-child-rtl', get_stylesheet_directory_uri() . '/style_rtl.css' );
