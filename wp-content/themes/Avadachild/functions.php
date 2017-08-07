d69c6a5ea3de59ce1ba68e7d40335ddda3e01308
Sutinai Jaradwilaswanit <The-nai@hotmail.com>
2017-08-07 12:16:16 +0700

    Remove Theme

--- wp-content/themes/Avadachild/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles',11 );
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/css/index.css');
-
-}
